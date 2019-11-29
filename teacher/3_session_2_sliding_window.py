214f7c19f8714af38e71ba39dbf46264d2c7ffb6
Florient CHOUTEAU <florient.f.chouteau@airbus.com>
2019-11-29 17:51:30 +0100

    Update notebooks

--- teacher/3_session_2_sliding_window.py
+++ teacher/3_session_2_sliding_window.py
@@ -16,7 +16,13 @@
 
 # %% [markdown]
 # # Session 2 Part 2: Using the sliding window technique to predict on larger images
-# In this session we will load our previously detected model and apply it on large images using the sliding window technique
+# In this session we will load our previously detected model and apply it on large images using the sliding window technique.
+#
+# The sliding window technique is a method to convert a classifier into detector. It can be illustrated by a single gif:
+#
+# ![](slides/static/img/sliding_window.gif)
+#
+# For more information about the sliding window technique
 # %%
 # Put your imports here
 import numpy as np
@@ -28,9 +34,32 @@ tiles_dataset_url = "https://storage.googleapis.com/isae-deep-learning/tiles_air
 # %% {"tags": ["exercise"]}
 # This cell should not be exported
 
+# %% [markdown]
+# ## Downloading the dataset
+
 # %%
 # Download data
 ds = np.DataSource("/tmp/")
 f = ds.open(tiles_dataset_url, 'rb')
 eval_tiles = np.load(f)
 eval_tiles = eval_tiles['eval_tiles']
+
+# %% [markdown]
+# ## Data Exploration
+
+# %%
+
+# %% [markdown]
+# ## Reloading your model
+
+# %%
+
+# %% [markdown]
+# ## Coding the sliding window
+
+# %%
+
+# %% [markdown]
+# ## Applying on the dataset
+
+# %%
